e377dd59ff722d164f3da82ab00bfdf11f33722c
KHaZzTuKBa <zamock15@gmail.com>
2024-04-11 13:55:20 +0300

    lab1

---
+++ laba_1/laba1_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDateTimeFormatter dateTimeFormatter;
+            AmericanDateTime americanDateTime = new AmericanDateTime();
+            EuropeanDateTime europeanDateTime = new EuropeanDateTime();
+
+            Console.WriteLine($"Американский формат: {americanDateTime.FormatDateTime()}");
+            Console.WriteLine($"Европейский формат: {europeanDateTime.FormatDateTime()}");
+            Console.WriteLine();
+
+            dateTimeFormatter = new EuropeanDateTime();
+
+            dateTimeFormatter = new SymbolDateTimeDecorator(dateTimeFormatter, "Ник");
+            dateTimeFormatter = new SymbolDateTimeDecorator(dateTimeFormatter, "Зам");
+
+            Console.WriteLine($"Декоратор: {dateTimeFormatter.FormatDateTime()}");
+        }
+    }
+}
